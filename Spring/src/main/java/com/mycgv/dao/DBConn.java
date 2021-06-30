fe7c3c452bfcc58f6fcf4073bca99b950d80df2f
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-06-30 16:42:14 +0900

    Revert "연동"
    This reverts commit 1b4a28200f8738086bee01899abc2b7962f76020.

--- Spring/src/main/java/com/mycgv/dao/DBConn.java
+++ Spring/src/main/java/com/mycgv/dao/DBConn.java
@@ -8,33 +8,37 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class DBConn {
-	String url;
+	// Field
+	String url = "jdbc:oracle:thin:@localhost:1521";
 	String user = "scott";
 	String pass = "tiger";
 	Connection conn;
+	Statement stmt;
 	PreparedStatement pstmt;
 	ResultSet rs;
 
 	// Constructor
 	public DBConn() {
 		try {
+			// 1단계 - 드라이버 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			// 운영체제 확인
-			String osName = System.getProperty("os.name").toLowerCase();
-			if (osName.indexOf("win") >= 0) { // 윈도우
-				url = "jdbc:oracle:thin:@127.0.0.1:1521";
-			} else if (osName.indexOf("mac") >= 0) { // 맥북
-				url = "jdbc:oracle:thin:@localhost:1521/xe";
-			}
-
+			// 2단계 - Connection 객체 생성
 			conn = DriverManager.getConnection(url, user, pass);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/** PreparedStatement 생성 **/
+	// Method
+	public void getStatement() {
+		try {
+			stmt = conn.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void getPreparedStatement(String sql) {
 		try {
 			pstmt = conn.prepareStatement(sql);
@@ -43,18 +47,16 @@ public class DBConn {
 		}
 	}
 
-	/** 종료 **/
+	// close
 	public void close() {
 		try {
-			if (rs != null) {
+			// 6단계 - 연결종료
+			if (rs != null)
 				rs.close();
-			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (conn != null) {
+			if (stmt != null)
+				stmt.close();
+			if (conn != null)
 				conn.close();
-			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
