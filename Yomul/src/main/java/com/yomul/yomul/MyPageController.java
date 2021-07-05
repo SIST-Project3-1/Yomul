ad543df6c7c750475aa6db87a1782cc92be385ea
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-05 15:18:49 +0900

    MyPageController: 마이페이지 해당 페이지들 매핑

--- Yomul/src/main/java/com/yomul/yomul/MyPageController.java
+++ Yomul/src/main/java/com/yomul/yomul/MyPageController.java
@@ -3,32 +3,57 @@ package com.yomul.yomul;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyPageController {
 
+	@RequestMapping(value = "/myarticle_list", method = RequestMethod.GET)
+	public ModelAndView myacticle_list() {
+		ModelAndView mv = new ModelAndView("user/mypage/myarticle_list");
+		mv.addObject("headerType", "myarticle");
+		return mv;
+	}
+
+	@RequestMapping(value = "/mycomment_list", method = RequestMethod.GET)
+	public ModelAndView mycomment_list() {
+		ModelAndView mv = new ModelAndView("user/mypage/mycomment_list");
+		mv.addObject("headerType", "myarticle");
+		return mv;
+	}
+
 	@RequestMapping(value = "/buy_list", method = RequestMethod.GET)
-	public String buy_list() {
-		return "user/mypage/buy_list";
+	public ModelAndView buy_list() {
+		ModelAndView mv = new ModelAndView("user/mypage/buy_list");
+		mv.addObject("headerType", "buy_list");
+		return mv;
 	}
 
 	@RequestMapping(value = "/sell_list", method = RequestMethod.GET)
-	public String sell_list() {
-		return "user/mypage/sell_list";
+	public ModelAndView sell_list() {
+		ModelAndView mv = new ModelAndView("user/mypage/sell_list");
+		mv.addObject("headerType", "buy_list");
+		return mv;
 	}
 
 	@RequestMapping(value = "/favorite_list", method = RequestMethod.GET)
-	public String favorite_list() {
-		return "user/mypage/favorite_list";
+	public ModelAndView favorite_list() {
+		ModelAndView mv = new ModelAndView("user/mypage/favorite_list");
+		mv.addObject("headerType", "buy_list");
+		return mv;
 	}
 
 	@RequestMapping(value = "myprofile_update", method = RequestMethod.GET)
-	public String myprofile_update() {
-		return "user/mypage/myprofile_update";
+	public ModelAndView myprofile_update() {
+		ModelAndView mv = new ModelAndView("user/mypage/myprofile_update");
+		mv.addObject("headerType", "myprofile");
+		return mv;
 	}
 
 	@RequestMapping(value = "myprofile_info", method = RequestMethod.GET)
-	public String myprofile_info() {
-		return "user/mypage/myprofile_info";
+	public ModelAndView myprofile_info() {
+		ModelAndView mv = new ModelAndView("user/mypage/myprofile_info");
+		mv.addObject("headerType", "myprofile");
+		return mv;
 	}
 }
