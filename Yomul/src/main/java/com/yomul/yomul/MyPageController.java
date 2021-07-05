fab232492d23f7813abea4d5c1dd3a178c1cb80e
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-05 14:15:08 +0900

    MyPageController: 구매내역, 판매내역, 찜 목록 매핑

--- Yomul/src/main/java/com/yomul/yomul/MyPageController.java
+++ Yomul/src/main/java/com/yomul/yomul/MyPageController.java
@@ -6,7 +6,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyPageController {
-	
+
+	@RequestMapping(value = "/buy_list", method = RequestMethod.GET)
+	public String buy_list() {
+		return "user/mypage/buy_list";
+	}
+
+	@RequestMapping(value = "/sell_list", method = RequestMethod.GET)
+	public String sell_list() {
+		return "user/mypage/sell_list";
+	}
+
+	@RequestMapping(value = "/favorite_list", method = RequestMethod.GET)
+	public String favorite_list() {
+		return "user/mypage/favorite_list";
+	}
+
 	@RequestMapping(value = "myprofile_update", method = RequestMethod.GET)
 	public String myprofile_update() {
 		return "user/mypage/myprofile_update";
