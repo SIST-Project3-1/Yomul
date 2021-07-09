2c473906e5aa29979e40e208bb424695127840a0
sayskpays <76785132+sayskpays@users.noreply.github.com>
2021-07-09 18:25:25 +0900

    내근처 게시글 등록 추가
    -- web.xml
    POST 방식으로 한글 데이터 넘길때 깨져서 인코딩 utf-8로 해주는 코드 추가
    
    --admin_faq_list
    faq 게시글 등록버튼 추가
    
    nearVO, DAO 추가
    사용자 내근처 게시글 등록

--- Yomul/src/main/java/com/yomul/yomul/NearController.java
+++ Yomul/src/main/java/com/yomul/yomul/NearController.java
@@ -5,14 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yomul.dao.NearDAO;
+import com.yomul.vo.NearVO;
+
 @Controller
 public class NearController {
-	//내 근처 기본화면
 	@RequestMapping(value="/near_home", method=RequestMethod.GET)
 	public ModelAndView near_home() {
 		ModelAndView mv = new ModelAndView();
 		
-		String keyword[] = { "부동산", "카페", "요가", "휴대폰", "마사지", "미용실", "왁싱" };
 		
 		mv.setViewName("user/near/near_home");
 		mv.addObject("keyword",keyword);
@@ -20,37 +21,51 @@ public class NearController {
 		return mv;
 	}
 	
-	//내 근처 글 쓰기 (동네구인구직, 과외/클래스, 농수산물, 중고차)
 	@RequestMapping(value="/near_write", method=RequestMethod.GET)
 	public String near_write() {
 		return "user/near/near_write";
 	}
 	
-	//내 근처 글 수정 (동네구인구직, 과외/클래스, 농수산물, 중고차)
+	@RequestMapping(value="/near_write_proc", method=RequestMethod.POST)
+	public ModelAndView near_write_proc(NearVO vo) {
+		int result;
+		ModelAndView mv = new ModelAndView();
+		NearDAO dao = new NearDAO();
+		
+		result = dao.getNearWrite(vo);
+		if(result==1) {
+			
+			mv.setViewName("redirect:/near_home");
+			
+		}else {
+			// mv.setViewName("error"); 에러페이지
+		}
+		
+		
+		return mv;
+	}
+	
+	//�궡 洹쇱쿂 湲� �닔�젙 (�룞�꽕援ъ씤援ъ쭅, 怨쇱쇅/�겢�옒�뒪, �냽�닔�궛臾�, 以묎퀬李�)
 	@RequestMapping(value="/near_update", method=RequestMethod.GET)
 	public String near_update() {
 		return "user/near/near_update";
 	}
 	
-	//내 근처 글 상세보기
 	@RequestMapping(value="/near_info", method=RequestMethod.GET)
 	public String near_info() {
 		return "user/near/near_info";
 	}
 	
-	//카테고리 폼 - 뉴스
 	@RequestMapping(value="/near_news_form", method=RequestMethod.GET)
 	public String near_news_form() {
 		return "user/near/near_news_form";
 	}
 	
-	//카테고리 폼 - 카드
 	@RequestMapping(value="/near_card_form", method=RequestMethod.GET)
 	public String near_card_form() {
 		return "user/near/near_card_form";
 	}
 	
-	//단골 후기 상세보기
 	@RequestMapping(value="/reviews_info", method=RequestMethod.GET)
 	public String reviews_info() {
 		return "user/near/reviews_info";
