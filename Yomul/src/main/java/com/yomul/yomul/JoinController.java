1f30e341d210b6855ff016f84a6149c4aef6280c
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-09 18:32:03 +0900

    회원가입: 이메일 중복 체크
    JoinController.java
    - email_check(): 이메일 중복 확인 메소드
    
    join.jsp
    - 중복확인 버튼에 ajax 로 중복체크 처리

--- Yomul/src/main/java/com/yomul/yomul/JoinController.java
+++ Yomul/src/main/java/com/yomul/yomul/JoinController.java
@@ -3,12 +3,29 @@ package com.yomul.yomul;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JoinController {
 
+	/**
+	 * 회원가입 페이지
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "join", method = RequestMethod.GET)
 	public String join() {
 		return "user/join/join";
+	}
+
+	/**
+	 * 이메일 중복 확인
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/email_check", method = RequestMethod.GET)
+	public String email_check(String email) {
+		return "1";
 	}
 }
