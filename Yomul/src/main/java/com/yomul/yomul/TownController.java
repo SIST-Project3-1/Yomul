8fb42400494920ce0a97ffbe50085bd02c6f2180
ohdavi <74041787+ohdavi@users.noreply.github.com>
2021-07-15 21:17:19 +0900

    동네생활 글쓰기 DB 연동
    town_write.jsp
    -파일선택 후 이미지 표시
    
    TownController.java
    -town_write_proc 만듦
    
    TownDAO.java
    -글쓰기 데이터 넣기
    
    TownVO.java
    -동네생활 데이터 getter, setter 만듦

--- Yomul/src/main/java/com/yomul/yomul/TownController.java
+++ Yomul/src/main/java/com/yomul/yomul/TownController.java
@@ -1,34 +1,57 @@
 package com.yomul.yomul;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.yomul.dao.TownDAO;
+import com.yomul.vo.FileVO;
+import com.yomul.vo.TownVO;
 
 @Controller
 public class TownController {
 
-	@RequestMapping(value = "town_list", method = RequestMethod.GET)
+	@RequestMapping(value = "/town_list", method = RequestMethod.GET)
 	public String town_list() {
 		return "user/town/town_list";
 	}
-
-	@RequestMapping(value = "town_write", method = RequestMethod.GET)
-	public String town_write() {
-		return "user/town/town_write";
-	}
-
-	@RequestMapping(value = "town_update", method = RequestMethod.GET)
+	
+	@RequestMapping(value = "/town_update", method = RequestMethod.GET)
 	public String town_update() {
 		return "user/town/town_update";
 	}
 
-	@RequestMapping(value = "town_info", method = RequestMethod.GET)
+	@RequestMapping(value = "/town_info", method = RequestMethod.GET)
 	public String town_info() {
 		return "user/town/town_info";
 	}
 
-	@RequestMapping(value = "town_write_api", method = RequestMethod.GET)
-	public String town_write_api() {
-		return "user/town/town_write_api";
+	@RequestMapping(value = "/town_write", method = RequestMethod.GET)
+	public String town_write() {
+		return "user/town/town_write";
 	}
+	
+	@RequestMapping(value = "/town_write_proc", method = RequestMethod.GET)
+	public ModelAndView town_write_proc(String category, String title, String content, String file) {
+		ModelAndView mv = new ModelAndView();
+		TownDAO dao = new TownDAO();
+		TownVO vo = new TownVO();
+		FileVO fvo = new FileVO();
+		vo.setCategory(category);
+		vo.setTitle(title);
+		vo.setContent(content);
+		fvo.setFilename(file);
+		boolean result = dao.getTownWrite(vo,fvo);
+		if(result) {
+			mv.setViewName("redirect:/town_list");
+		}else {
+			// mv.setViewName("error"); 에러페이지
+		}
+		return mv;
+	}
+	
+	
 }
