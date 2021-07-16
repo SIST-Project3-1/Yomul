ebea674f6c1fa9126a402434020d56222fc3c085
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-16 16:31:40 +0900

    Update NearService.java

--- Yomul/src/main/java/com/yomul/service/NearService.java
+++ Yomul/src/main/java/com/yomul/service/NearService.java
@@ -1,14 +1,19 @@
 package com.yomul.service;
 
+import java.util.List;
+
 import com.yomul.vo.NearVO;
 
 public interface NearService {
-	//내근처 글쓰기
-	public int getNearWrite(NearVO vo);
-	
+	// 내근처 글쓰기
+	int getNearWrite(NearVO vo);
+
+	// 내 근처 홈 화면 보기
+	List<NearVO> selectNearList(NearVO vo);
+
 	// 내 근처 게시글 상세보기
-	public NearVO getNearInfo(String no);
-	
+	NearVO getNearInfo(String no);
+
 	// 내 근처 게시글 조회수 업데이트
-	public int updateNearHits(String no);
+	int updateNearHits(String no);
 }
