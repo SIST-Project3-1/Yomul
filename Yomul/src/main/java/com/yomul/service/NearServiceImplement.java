780b10fefab5ef48187d2b791cd9eb1d52fa4ff6
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-16 16:32:10 +0900

    Update NearServiceImplement.java

--- Yomul/src/main/java/com/yomul/service/NearServiceImplement.java
+++ Yomul/src/main/java/com/yomul/service/NearServiceImplement.java
@@ -1,5 +1,7 @@
 package com.yomul.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,14 +9,14 @@ import com.yomul.dao.NearDAO;
 import com.yomul.vo.NearVO;
 
 @Service("nearService")
-public class NearServiceImplement implements NearService{
+public class NearServiceImplement implements NearService {
 
 	@Autowired
 	private NearDAO dao;
-	
+
 	@Override
 	public int getNearWrite(NearVO vo) {
-		
+
 		return dao.getNearWrite(vo);
 	}
 
@@ -22,9 +24,15 @@ public class NearServiceImplement implements NearService{
 	public NearVO getNearInfo(String no) {
 		return dao.getNearInfo(no);
 	}
-	
+
 	@Override
 	public int updateNearHits(String no) {
 		return dao.updateNearHits(no);
+	}
+
+	@Override
+	public List<NearVO> selectNearList(NearVO vo) {
+
+		return dao.getList(vo);
 	}
 }
