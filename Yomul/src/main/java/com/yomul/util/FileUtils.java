7e82f7dd4c7f4a9132e58ffee5a7710ec6c747e7
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-16 16:33:05 +0900

    Update FileUtils.java

--- Yomul/src/main/java/com/yomul/util/FileUtils.java
+++ Yomul/src/main/java/com/yomul/util/FileUtils.java
@@ -25,7 +25,23 @@ public class FileUtils {
 
 	@Autowired
 	private FileService fileService;
-	
+
+	private static long SEQUENCE = 0L;
+
+	// 저장하는 파일 이름을 구하는 함수
+	public static String getFileName(String articleNo, MultipartFile file) {
+		return articleNo + "_1_" + file.getOriginalFilename();
+	}
+
+	// 저장하는 파일 이름을 구하는 함수(파일 여러 개)
+	public static String[] getFileName(String articleNo, MultipartFile[] files) {
+		String[] result = new String[files.length];
+		for (int i = 1; i <= files.length; i++) {
+			result[i] = articleNo + "_" + i + "_" + files[i].getOriginalFilename();
+		}
+		return result;
+	}
+
 	// 파일 저장 경로를 구하는 함수
 	public static String getUploadPath(HttpServletRequest request) {
 		return request.getSession().getServletContext().getRealPath("/resources/upload");
@@ -45,9 +61,9 @@ public class FileUtils {
 		}
 		return result;
 	}
-	
-	// 내근처 글쓰기 파일업로드 
-	//파일업로드 갯수 확인
+
+	// 내근처 글쓰기 파일업로드
+	// 파일업로드 갯수 확인
 	public int getUploadedCount(List<MultipartFile> multipartFile) {
 		int count = 0;
 		for (MultipartFile mf : multipartFile) {
@@ -57,33 +73,34 @@ public class FileUtils {
 		}
 		return count;
 	}
-	public String restore(List<MultipartFile> multipartFile,NearDAO dao, HttpServletRequest request) {
+
+	public String restore(List<MultipartFile> multipartFile, NearDAO dao, HttpServletRequest request, String articleNo) {
 		String url = null;
 		final String PREFIX_URL = "resources/upload/";
 		try {
 			// 파일 정보
 			for (MultipartFile mf : multipartFile) {
-				//원파일명
+				// 원파일명
 				String originFilename = mf.getOriginalFilename();
-				//변환 파일명
+				// 변환 파일명
 				String extName = originFilename.substring(originFilename.lastIndexOf("."), originFilename.length());
-				//파일 크기
+				// 파일 크기
 				Long size = mf.getSize();
-				
+
 				// 서버에서 저장 할 파일 이름
-				String saveFileName = genSaveFileName(extName);
-				//db에 저장
-				dao.getNearFile(saveFileName,originFilename);
-				
+				String saveFileName = genSaveFileName(articleNo, extName);
+				// db에 저장
+				dao.getNearFile(saveFileName, originFilename);
+
 				System.out.println("originFilename : " + originFilename);
 				System.out.println("extensionName : " + extName);
 				System.out.println("size : " + size);
 				System.out.println("saveFileName : " + saveFileName);
-				
+
 				writeFile(mf, saveFileName, request);
 				url = PREFIX_URL + saveFileName;
-				
-				System.out.println("url :" + url );
+
+				System.out.println("url :" + url);
 			}
 
 		} catch (IOException e) {
@@ -92,9 +109,16 @@ public class FileUtils {
 		}
 		return url;
 	}
+
 	// 중복되지 않게 현재 시간 붙여서 파일 이름 생성
+	public String genSaveFileName(String articleNo, String extName) {
+		String fileName = articleNo + "_" + (++SEQUENCE) + "_" + extName;
+
+		return fileName;
+	}
+
 	public String genSaveFileName(String extName) {
-	
+
 		String fileName = "near";
 		Calendar calendar = Calendar.getInstance();
 		fileName += calendar.get(Calendar.YEAR);
@@ -105,7 +129,6 @@ public class FileUtils {
 		fileName += calendar.get(Calendar.SECOND);
 		fileName += calendar.get(Calendar.MILLISECOND);
 		fileName += extName;
-
 		return fileName;
 	}
 
@@ -114,16 +137,15 @@ public class FileUtils {
 		final String SAVE_PATH = request.getSession().getServletContext().getRealPath("/resources/upload");
 		System.out.println("save_path ===> " + SAVE_PATH);
 		boolean result = false;
-		
-			byte[] data = mf.getBytes();
-			System.out.println("data : " + data);
-			
-			FileOutputStream fos = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
-			fos.write(data);
-			fos.close();
-		
-		
+
+		byte[] data = mf.getBytes();
+		System.out.println("data : " + data);
+
+		FileOutputStream fos = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
+		fos.write(data);
+		fos.close();
+
 		return result;
 	}
-	
+
 }
