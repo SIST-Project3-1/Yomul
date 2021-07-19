b1dc71bf89ff9dee2e085db6f2943fe0f9902722
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2021-07-19 16:17:14 +0900

    내 근처 관리: 페이지 데이터 불러오기 및 링크 구현
    NearDAO.java
    - getNearList(): 페이지당 10개씩 글을 불러오는 메소드
    
    near.xml
    - getNearList: 맵핑
    
    NearService.java
    - 여러 dao를 이용해서 데이터를 처리할 수 있으므로dao -> nearDAO
    - getNearList() 추가
    
    NearVO.java
    - int 형 파라미터 기본 값 -1 설정
    
    AdminController.java
    - admin_nearhome(): MAV 로 리턴타입 변경
    
    NearController.java
    - 코드 정렬
    - near_home_ajax(): 내 근처 글 목록을 AJAX로 불러오는 페이지 맵핑
    
    admin_near_home.jsp
    - 페이지 불러오기 AJAX 처리
    - 페이지 UI 변경
    - 테이블 속성 추가: 번호,  분류
    - css 간결화
    
    Oracle_Yomul.sql
    - 테스트 데이터 추가
    - 내 근처 글 목록 불러오기 쿼리 추가

--- Yomul/src/main/java/com/yomul/yomul/NearController.java
+++ Yomul/src/main/java/com/yomul/yomul/NearController.java
@@ -35,22 +35,22 @@ public class NearController {
 
 	@Autowired
 	private NearService nearService;
-	
+
 	@Autowired
 	private FileService fileService;
-	
+
 	@Autowired
 	private CommentService commentService;
-	
+
 	@Autowired
 	private VendorService vendorService;
-	
+
 	@Autowired
 	private LikeService likeService;
-	
+
 	@Autowired
 	private ReportService reportService;
-	
+
 	@Autowired
 	private NearDAO nearDAO;
 
@@ -59,7 +59,7 @@ public class NearController {
 
 	@RequestMapping(value = "/near_home", method = RequestMethod.GET)
 	public ModelAndView near_home(NearVO vo) {
-		
+
 		ModelAndView mv = new ModelAndView();
 		List<NearVO> list = nearService.selectNearList(vo);
 		String keyword[] = { "부동산", "카페", "요가", "휴대폰", "마사지", "미용실", "왁싱" };
@@ -71,6 +71,19 @@ public class NearController {
 		return mv;
 	}
 
+	/**
+	 * 내 근처 글 목록 AJAX 불러오기
+	 * 
+	 * @param near
+	 * @param page
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/near_home_ajax", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	public String near_home_ajax(NearVO near, String page) {
+		return Commons.parseJson(nearService.getNearList(near, page));
+	}
+
 	// 내 근처 글 쓰기 (동네구인구직, 과외/클래스, 농수산물, 중고차)
 	@RequestMapping(value = "/near_write", method = RequestMethod.GET)
 	public String near_write() {
@@ -78,17 +91,18 @@ public class NearController {
 	}
 
 	@RequestMapping(value = "/near_write_proc", method = RequestMethod.POST)
-	public ModelAndView near_write_proc(NearVO vo, @RequestParam("uploadFile") List<MultipartFile> files, HttpServletRequest request, HttpSession session) {
+	public ModelAndView near_write_proc(NearVO vo, @RequestParam("uploadFile") List<MultipartFile> files, HttpServletRequest request,
+			HttpSession session) {
 
 		ModelAndView mv = new ModelAndView();
 		int fileCount = fileUploadService.getUploadedCount(files);
 		String articleNo = nearDAO.getWriteNumber();
-		if(fileCount !=0) {
-			String url = fileUploadService.restore(files, nearDAO, request, articleNo);			
+		if (fileCount != 0) {
+			String url = fileUploadService.restore(files, nearDAO, request, articleNo);
 			mv.addObject("url", url);
 		}
 		mv.addObject("fileCount", fileCount);
-		vo.setWriter(((MemberVO)session.getAttribute("member")).getNo());
+		vo.setWriter(((MemberVO) session.getAttribute("member")).getNo());
 		int result = nearDAO.getNearWrite(vo);
 
 		if (result == 1) {
@@ -115,49 +129,49 @@ public class NearController {
 		// 조회수 갱신 겸 게시글 유무 확인
 		if (nearService.updateNearHits(no) != 0) {
 			mv.setViewName("user/near/near_info");
-			
+
 			// 게시글 정보 불러오기
 			NearVO vo = nearService.getNearInfo(no);
 			mv.addObject("vo", vo);
-			
+
 			// 게시글 파일이 있을 경우 불러오기
-			if(vo.getFiles() != 0) {
+			if (vo.getFiles() != 0) {
 				ArrayList<String> files = fileService.getArticleFiles(no);
 				mv.addObject("articleImages", files);
 			}
-			
+
 			// 댓글 정보 불러오기
 			int commentCount = commentService.getCommentCount(no);
 			ArrayList<CommentVO> comments = commentService.getCommentList(no, 1);
 			mv.addObject("comments", comments);
-			
+
 			// 댓글 페이지 정보 불러오기
 			HashMap<String, Integer> commentPageInfo = Commons.getPageInfo(commentCount, 10);
 			mv.addObject("commentPageInfo", commentPageInfo);
-			
+
 			// 단골 정보 불러오기
 			int vendorCustomerCount = vendorService.getVendorCustomerCount(vo.getVno());
 			mv.addObject("vendorCustomerCount", vendorCustomerCount);
-			
+
 		} else { // 게시글이 없을 경우 에러페이지 이동
 			mv.setViewName("redirect:/user/error");
 		}
-		
+
 		return mv;
 	}
-	
+
 	// 단골 등록 ajax
 	@ResponseBody
 	@RequestMapping(value = "/near_info/add_vendor_customer_proc", method = RequestMethod.GET)
 	public String near_info_comment(String vno, HttpSession session) {
 		// 로그인한 계정 번호 불러오기
 		String mno = Commons.getMno(session);
-		if(mno.equals("")) { // 로그인이 안되어 있을 경우 -1 반환
+		if (mno.equals("")) { // 로그인이 안되어 있을 경우 -1 반환
 			return "-1";
 		}
-		
+
 		int result = vendorService.switchVendorCustomer(vno, mno);
-		
+
 		return String.valueOf(result);
 	}
 
